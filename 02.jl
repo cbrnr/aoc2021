2a0e88fc41dae6f5ba602f56d23d7da874827c64
Clemens Brunner <clemens.brunner@gmail.com>
2021-12-03 09:07:49 +0100

    Fix soft scope issue

--- 02.jl
+++ 02.jl
@@ -5,34 +5,34 @@ input = get_aoc_input(2, cookie)
 data = split(strip(input), "\n")
 
 # part 1
-horizontal, depth = 0, 0
-for command in data
-    direction, units = split(command)
-    units = parse(Int, units)
-    if direction == "forward"
-        horizontal += units
-    elseif direction == "down"
-        depth += units
-    elseif direction == "up"
-        depth -= units
+let horizontal = 0, depth = 0
+    for command in data
+        direction, units = split(command)
+        units = parse(Int, units)
+        if direction == "forward"
+            horizontal += units
+        elseif direction == "down"
+            depth += units
+        elseif direction == "up"
+            depth -= units
+        end
     end
+    println("Answer 1: ", horizontal * depth)
 end
-
-println("Answer 1: ", horizontal * depth)
 
 # part 2
-aim, horizontal, depth = 0, 0, 0
-for command in data
-    direction, units = split(command)
-    units = parse(Int, units)
-    if direction == "forward"
-        horizontal += units
-        depth += aim * units
-    elseif direction == "down"
-        aim += units
-    elseif direction == "up"
-        aim -= units
+let aim = 0, horizontal = 0, depth = 0
+    for command in data
+        direction, units = split(command)
+        units = parse(Int, units)
+        if direction == "forward"
+            horizontal += units
+            depth += aim * units
+        elseif direction == "down"
+            aim += units
+        elseif direction == "up"
+            aim -= units
+        end
     end
+    println("Answer 2: ", horizontal * depth)
 end
-
-println("Answer 2: ", horizontal * depth)
