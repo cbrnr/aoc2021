a8e1bf43467864cd8b92bb4dc582487f23966665
Clemens Brunner <clemens.brunner@gmail.com>
2021-12-03 08:21:46 +0100

    Move get_aoc_input to separate file

--- 02.jl
+++ 02.jl
@@ -1,19 +1,4 @@
-using HTTP
-
-"""
-    get_aoc_input(day, cookie)
-
-Download Advent of Code 2021 input data for the given `day`.
-
-A valid `cookie` must be provided, for example by logging into the
-[Advent of Code website](https://adventofcode.com/2021) with a browser and copying the
-session cookie (accessible in the browser preferences).
-"""
-function get_aoc_input(day::Integer, cookie::AbstractString)
-    cookies = Dict("session"=>cookie)
-    r = HTTP.get("https://adventofcode.com/2021/day/$day/input", cookies=cookies)
-    String(r.body)
-end
+include("utils.jl")
 
 cookie = ""
 input = get_aoc_input(2, cookie)
