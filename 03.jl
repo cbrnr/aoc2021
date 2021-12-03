57b323811810d1126d1472b17ba849a48ba97c12
Clemens Brunner <clemens.brunner@gmail.com>
2021-12-03 11:14:11 +0100

    Use permutedims for better readability

--- 03.jl
+++ 03.jl
@@ -5,8 +5,8 @@ input = get_aoc_input(3, cookie)
 data = parse.(Int16, split(strip(input)), base=2)
 
 # part 1
-report = hcat(digits.(data, base=2, pad=ndigits(maximum(data), base=2))...)
-gamma_bits = reverse(sum(report, dims=2) .> size(report, 2) / 2)
+report = permutedims(hcat(digits.(data, base=2, pad=ndigits(maximum(data), base=2))...))
+gamma_bits = reverse(sum(report, dims=1) .> size(report, 1) / 2)
 gamma = parse(Int, join(string.(gamma_bits, base=2)), base=2)
 epsilon = parse(Int, join(string.(.!gamma_bits, base=2)), base=2)
 
