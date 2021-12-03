27ffb383c9b02971cd916c93d0538e122050c366
Clemens Brunner <clemens.brunner@gmail.com>
2021-12-03 10:51:49 +0100

    No need for BitArray

--- 03.jl
+++ 03.jl
@@ -5,7 +5,7 @@ input = get_aoc_input(3, cookie)
 data = parse.(Int16, split(strip(input)), base=2)
 
 # part 1
-report = BitArray(hcat(digits.(data, base=2, pad=ndigits(maximum(data), base=2))...))
+report = hcat(digits.(data, base=2, pad=ndigits(maximum(data), base=2))...)
 gamma_bits = reverse(sum(report, dims=2) .> size(report, 2) / 2)
 gamma = parse(Int, join(string.(gamma_bits, base=2)), base=2)
 epsilon = parse(Int, join(string.(.!gamma_bits, base=2)), base=2)
