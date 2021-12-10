05efbbca1b81f184d3dac39e1c597bcaf67b0c1b
Clemens Brunner <clemens.brunner@gmail.com>
2021-12-10 08:20:33 +0100

    Finish day 9 part 2

--- 09.jl
+++ 09.jl
@@ -22,3 +22,24 @@ end
 
 lowpoints = find_lowpoints(heightmap)
 println("Answer 1: ", sum(1 + heightmap[i...] for i in lowpoints))
+
+function basin_size(heightmap, lowpoint)
+    heightmap = copy(heightmap)
+    counter = 0
+    queue = [lowpoint]
+    nrows, ncols = size(heightmap)
+    while !isempty(queue)
+        i, j = popfirst!(queue)
+        if heightmap[i, j] < 9
+            heightmap[i, j] = 10
+            counter += 1
+            i > 1 && push!(queue, (i - 1, j))
+            i < nrows && push!(queue, (i + 1, j))
+            j > 1 && push!(queue, (i, j - 1))
+            j < ncols && push!(queue, (i, j + 1))
+        end
+    end
+    counter
+end
+
+println("Answer 2: ", prod(sort(basin_size.(Ref(heightmap), lowpoints), rev=true)[1:3]))
