e4ec74b8fd12e03154389ecc875e4653b53701a7
Clemens Brunner <clemens.brunner@gmail.com>
2021-12-13 13:32:55 +0100

    Finish day 13

--- 13.jl
+++ 13.jl
@@ -3,7 +3,7 @@ include("utils.jl")
 cookie = ""
 input = get_aoc_input(13, cookie)
 
-part1, part2 = split.(split(input, "\n\n"), "\n")
+part1, part2 = split.(split(strip(input), "\n\n"), "\n")
 dots = reverse(permutedims(hcat([parse.(Int, element) for element in split.(part1, ",")]...)), dims=2) .+ 1
 instructions = replace.(part2, "fold along " => "")
 
@@ -18,17 +18,37 @@ end
 
 grid = create_grid(dots)
 
-function fold_grid(grid, instruction)
+function fold_grid(grid, instructions)
     grid = copy(grid)
-    axis, loc = split(instruction, "=")
-    loc = parse(Int, loc) + 1
-    if axis == "y"  # fold up
-        folded = grid[1:loc - 1, :] .+ reverse(grid[loc + 1:end, :], dims=1)
-    elseif axis == "x"  # fold left
-        folded = grid[:, 1:loc - 1] .+ reverse(grid[:, loc + 1:end], dims=2)
+    for instruction in instructions
+        axis, loc = split(instruction, "=")
+        loc = parse(Int, loc) + 1
+        if axis == "y"  # fold up
+            folded = grid[1:loc - 1, :] .+ reverse(grid[loc + 1:end, :], dims=1)
+        elseif axis == "x"  # fold left
+            folded = grid[:, 1:loc - 1] .+ reverse(grid[:, loc + 1:end], dims=2)
+        end
+        folded[folded .> 1] .= 1
+        grid = folded
     end
-    folded[folded .> 1] .= 1
-    folded
+    grid
 end
 
-println("Answer 1: ", sum(fold_grid(grid, instructions[1])))
+function print_grid(grid)
+    s = ""
+    for x in eachrow(code)
+        for y in x
+            if y == 0
+                s *= " "
+            else
+                s *= "▮"
+            end
+        end
+        s *= "\n"
+    end
+    println(s)
+end
+
+println("Answer 1: ", sum(fold_grid(grid, [instructions[1]])))
+println("Answer 2:")
+print_grid(fold_grid(grid, instructions))
--- README.md
+++ README.md
@@ -13,3 +13,4 @@ This repository contains my solutions for [Advent of Code 2021](https://adventof
 - [Day 10](https://github.com/cbrnr/aoc2021/blob/main/10.jl)
 - [Day 11](https://github.com/cbrnr/aoc2021/blob/main/11.jl)
 - [Day 12](https://github.com/cbrnr/aoc2021/blob/main/12.jl)
+- [Day 13](https://github.com/cbrnr/aoc2021/blob/main/13.jl)
